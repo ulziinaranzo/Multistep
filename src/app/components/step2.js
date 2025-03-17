1db67950d3dab7dcc8a3425a5b97301e0b16ae34
ulziinaranzo <ulziinaranzorig@gmail.com>
2025-03-17 13:18:18 +0800

    lol

--- src/app/components/step2.js
+++ src/app/components/step2.js
@@ -1,28 +1,243 @@
+// "use client";
+// import { ArrowIcon } from "@/assets/Arrow-Icon";
+// import { useState } from "react";
+// import { useForm } from "react-hook-form";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { z } from "zod";
+
+// export const schema = z.object({
+//   date: z.string().min(1, { message: "Огноо оруулна уу" }),
+//   img: z.instanceof(File).refine(
+//     (file) => {
+//       console.log(file);
+//       const allowedExtensions = ["jpg", "jpeg", "png", "gif"];
+//       return allowedExtensions.includes(file[0].type.split("/")[1]);
+//     },
+//     { message: "Зурган файл оруулна уу" }
+//   ),
+// });
+
+// export const Step2 = ({ handleContinue }) => {
+//   const {
+//     register,
+//     formState: { errors },
+//     handleSubmit,
+//     watch,
+//   } = useForm({
+//     resolver: zodResolver(schema),
+//     defaultValues: {
+//       date: "",
+//       img: null,
+//     },
+//   });
+
+//   const { img } = watch();
+
+//   return (
+//     <div className="flex justify-center items-center h-screen w-screen bg-[#F4F4F4]">
+//       <form onSubmit={handleSubmit(handleContinue)}>
+//         <div className="flex flex-col relative w-[480px] h-fit rounded-[8px] justify-between p-[32px] bg-[white]">
+//           <div className="flex flex-col gap-[8px] mb-[28px]">
+//             <img src="/Images/Main 1.png" className="w-[60px] h-[60px]" />
+//             <div className="text-[26px] text-[#202124]">
+//               <b>Join Us! 😎</b>
+//             </div>
+//             <div className="text-[#8E8E8E] text-[18px]">
+//               Please provide all current information accurately.
+//             </div>
+//           </div>
+
+//           <div className="flex flex-col gap-[8px] text-[14px] text-[#334155]">
+//             <div className="flex gap-[4px] font-semibold">
+//               Date <div className="flex text-[#E14942]">*</div>
+//             </div>
+//             <input
+//               {...register("date")}
+//               type="date"
+//               className={`w-[416px] h-[44px] border p-[12px] rounded-[8px] ${
+//                 errors.date ? "border-red-600" : "border-[#8B8E95]"
+//               }`}
+//             />
+//             {errors.date && (
+//               <div className="text-red-600 text-sm">{errors.date.message}</div>
+//             )}
+//           </div>
+//           <div className="flex flex-col gap-[8px] text-[14px] text-[#334155] mt-[4px]">
+//             <div className="flex gap-[4px] font-semibold">
+//               Profile Image <div className="flex text-[#E14942]">*</div>
+//             </div>
+//             <input
+//               {...register("img")}
+//               type="file"
+//               className="flex flex-col justify-center items-center w-[416px] h-[180px] p-[12px] rounded-md mt-[12px] divide-none bg-[#7F7F800D]"
+//             />
+//             {errors.img && (
+//               <div className="text-red-600 text-sm">{errors.img.message}</div>
+//             )}
+//             {img && (
+//               <img
+//                 className="absolute top-[295px] left-[20px] w-[440px] h-[205px] p-[12px] rounded-[10px] mt-[12px] divide-none"
+//                 src={URL.createObjectURL(img[0])}
+//               />
+//             )}
+//           </div>
+
+//           <button
+//             type="submit"
+//             className="flex justify-center items-center bg-[#121316] rounded-[6px] text-[white] h-[44px] w-[416px] mt-[10px]"
+//           >
+//             Submit <ArrowIcon />
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+
+// "use client";
+// import { ArrowIcon } from "@/assets/Arrow-Icon";
+// import { useState } from "react";
+// import { useForm } from "react-hook-form";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { z } from "zod";
+
+// export const schema = z.object({
+//   date: z.string().min(1, { message: "Огноо оруулна уу" }),
+//   img: z.instanceof(File).refine(
+//     (file) => {
+//       console.log(file);
+//       const allowedExtensions = ["jpg", "jpeg", "png", "gif"];
+//       return allowedExtensions.includes(file[0].type.split("/")[1]);
+//     },
+//     { message: "Зурган файл оруулна уу" }
+//   ),
+// });
+
+// export const Step2 = ({ handleContinue }) => {
+//   const {
+//     register,
+//     formState: { errors },
+//     handleSubmit,
+//     watch,
+//     setValue,
+//   } = useForm({
+//     resolver: zodResolver(schema),
+//     defaultValues: {
+//       date: "",
+//       img: null,
+//     },
+//   });
+
+//   const { img } = watch();
+
+//   const onFileChange = (e) => {
+//     // Файлын өөрчлөлтийг хянах
+//     const file = e.target.files[0];
+//     if (file) {
+//       // Файлыг form-д шинэчилж хадгалах
+//       setValue("img", file);
+//     }
+//   };
+
+//   return (
+//     <div className="flex justify-center items-center h-screen w-screen bg-[#F4F4F4]">
+//       <form onSubmit={handleSubmit(handleContinue)}>
+//         <div className="flex flex-col relative w-[480px] h-fit rounded-[8px] justify-between p-[32px] bg-[white]">
+//           <div className="flex flex-col gap-[8px] mb-[28px]">
+//             <img src="/Images/Main 1.png" className="w-[60px] h-[60px]" />
+//             <div className="text-[26px] text-[#202124]">
+//               <b>Join Us! 😎</b>
+//             </div>
+//             <div className="text-[#8E8E8E] text-[18px]">
+//               Please provide all current information accurately.
+//             </div>
+//           </div>
+
+//           <div className="flex flex-col gap-[8px] text-[14px] text-[#334155]">
+//             <div className="flex gap-[4px] font-semibold">
+//               Date <div className="flex text-[#E14942]">*</div>
+//             </div>
+//             <input
+//               {...register("date")}
+//               type="date"
+//               className={`w-[416px] h-[44px] border p-[12px] rounded-[8px] ${
+//                 errors.date ? "border-red-600" : "border-[#8B8E95]"
+//               }`}
+//             />
+//             {errors.date && (
+//               <div className="text-red-600 text-sm">{errors.date.message}</div>
+//             )}
+//           </div>
+
+//           <div className="flex flex-col gap-[8px] text-[14px] text-[#334155] mt-[4px]">
+//             <div className="flex gap-[4px] font-semibold">
+//               Profile Image <div className="flex text-[#E14942]">*</div>
+//             </div>
+//             <input
+//               {...register("img")}
+//               type="file"
+//               onChange={onFileChange}
+//               className="w-[416px] h-[44px] p-[12px] rounded-md mt-[12px] bg-[#7F7F800D]"
+//             />
+//             {errors.img && (
+//               <div className="text-red-600 text-sm">{errors.img.message}</div>
+//             )}
+//             {img && (
+//               <div className="flex justify-center mt-4">
+//                 <img
+//                   className="w-[200px] h-[200px] object-cover rounded-[10px]"
+//                   src={URL.createObjectURL(img)}
+//                   alt="Uploaded Profile"
+//                 />
+//               </div>
+//             )}
+//           </div>
+
+//           <button
+//             type="submit"
+//             className="flex justify-center items-center bg-[#121316] rounded-[6px] text-[white] h-[44px] w-[416px] mt-[10px]"
+//           >
+//             Submit <ArrowIcon />
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+
+
 "use client";
 import { ArrowIcon } from "@/assets/Arrow-Icon";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { motion } from "framer-motion";
 
 export const schema = z.object({
   date: z.string().min(1, { message: "Огноо оруулна уу" }),
-  img: z.instanceof(File).refine(
-    (file) => {
-      console.log(file);
-      const allowedExtensions = ["jpg", "jpeg", "png", "gif"];
-      return allowedExtensions.includes(file[0].type.split("/")[1]);
-    },
-    { message: "Зурган файл оруулна уу" }
-  ),
+  img: z
+    .instanceof(File)
+    .refine(
+      (file) => {
+        const allowedExtensions = ["jpg", "jpeg", "png", "gif"];
+        return allowedExtensions.includes(file.type.split("/")[1]);
+      },
+      { message: "Зурган файл оруулна уу" }
+    ),
 });
 
 export const Step2 = ({ handleContinue }) => {
+  const [selectedFile, setSelectedFile] = useState(null); 
+
   const {
     register,
     formState: { errors },
     handleSubmit,
     watch,
+    setValue,
   } = useForm({
     resolver: zodResolver(schema),
     defaultValues: {
@@ -31,11 +246,37 @@ export const Step2 = ({ handleContinue }) => {
     },
   });
 
-  const { img } = watch();
+
+  const onFileChange = (e) => {
+    const file = e.target.files[0]; 
+    if (file) {
+      setSelectedFile(file); 
+      setValue("img", file); 
+    }
+  };
+
+  const onSubmit = (data) => {
+    console.log("Form data submitted: ", data); 
+    handleContinue(data); 
+  };
+
+  const handleAnimation = {
+    initial: { opacity: 0, x: -100 },
+    animate: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: 100 },
+    transition: { duration: 0.5 },
+  };
 
   return (
-    <div className="flex justify-center items-center h-screen w-screen bg-[#F4F4F4]">
-      <form onSubmit={handleSubmit(handleContinue)}>
+    <motion.div
+      className="flex justify-center items-center h-screen w-screen bg-[#F4F4F4]"
+      variants={handleAnimation}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition="transition"
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col relative w-[480px] h-fit rounded-[8px] justify-between p-[32px] bg-[white]">
           <div className="flex flex-col gap-[8px] mb-[28px]">
             <img src="/Images/Main 1.png" className="w-[60px] h-[60px]" />
@@ -62,6 +303,7 @@ export const Step2 = ({ handleContinue }) => {
               <div className="text-red-600 text-sm">{errors.date.message}</div>
             )}
           </div>
+
           <div className="flex flex-col gap-[8px] text-[14px] text-[#334155] mt-[4px]">
             <div className="flex gap-[4px] font-semibold">
               Profile Image <div className="flex text-[#E14942]">*</div>
@@ -69,16 +311,20 @@ export const Step2 = ({ handleContinue }) => {
             <input
               {...register("img")}
               type="file"
-              className="flex flex-col justify-center items-center w-[416px] h-[180px] p-[12px] rounded-md mt-[12px] divide-none bg-[#7F7F800D]"
+              onChange={onFileChange} 
+              className="w-[416px] h-[44px] p-[12px] rounded-md mt-[12px] bg-[#7F7F800D]"
             />
             {errors.img && (
               <div className="text-red-600 text-sm">{errors.img.message}</div>
             )}
-            {img && (
-              <img
-                className="absolute top-[295px] left-[20px] w-[440px] h-[205px] p-[12px] rounded-[10px] mt-[12px] divide-none"
-                src={URL.createObjectURL(img[0])}
-              />
+            {selectedFile && (
+              <div className="flex justify-center mt-4">
+                <img
+                  className="w-[200px] h-[200px] object-cover rounded-[10px]"
+                  src={URL.createObjectURL(selectedFile)} 
+                  alt="Uploaded Profile"
+                />
+              </div>
             )}
           </div>
 
@@ -90,6 +336,6 @@ export const Step2 = ({ handleContinue }) => {
           </button>
         </div>
       </form>
-    </div>
+      </motion.div>
   );
 };
